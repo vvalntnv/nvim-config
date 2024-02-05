9f1632897aad0c573ef30dbaa27613df85b633e2
vvalntnv <vvalntnv@gmail.com>
2024-02-05 09:51:10 +0200

    Inintial

---
+++ lua/viki/remap.lua
@@ -0,0 +1,11 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+vim.api.nvim_set_keymap("n", "<leader>w", ":w<CR>", { noremap = true, silent = true })
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+vim.keymap.set("x", "<leader>p", "\"_dP")
---
+++ lua/viki/set.lua
@@ -0,0 +1,9 @@
+vim.opt.guicursor = ""
+vim.opt.scrolloff = 8
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.expandtab = true
+
+vim.opt.smartindent = true
