0a47d0a7f06e45c00c20613d5799c6b6aeee37ed
vvalntnv <vvalntnv@gmail.com>
2024-02-19 06:51:42 +0200

    update

--- lua/viki/remap.lua
+++ lua/viki/remap.lua
@@ -62,3 +62,14 @@ end)
 -- nvim-dap-ui --
 vim.keymap.set("n", "<leader>od", function() require("dapui").open() end)
 vim.keymap.set("n", "<leader>cd", function() require("dapui").close() end)
+
+
+-- Copilot --
+
+vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
+	expr = true,
+	replace_keycodes = false
+})
+vim.g.copilot_no_tab_map = true
+vim.keymap.set('i', '<C-L>', '<Plug>(copilot-accept-word)')
+
--- lua/viki/set.lua
+++ lua/viki/set.lua
@@ -1,3 +1,4 @@
+vim.opt.guicursor = ""
 vim.opt.number = true
 vim.opt.relativenumber = true
 
