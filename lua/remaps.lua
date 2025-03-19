c265156c5eac516cff1f0cf87b79098e38d3ec70
vvalentinov <vvalentinov@neterra.net>
2025-03-19 10:07:13 +0200

    Harpoon added

--- lua/remaps.lua
+++ lua/remaps.lua
@@ -51,3 +51,10 @@ vim.keymap.set("n", "<leader>mm", function()
 	local copanel = require("copilot.panel")
 	copanel.open({ position = "bottom", ratio = 0.4 })
 end, { desc = "Make Mason" })
+
+
+-- Harpoon --
+vim.keymap.set('n', "<C-a>", ':lua require("harpoon.mark").add_file()<CR>')
+vim.keymap.set('n', "<C-l>", ':lua require("harpoon.ui").toggle_quick_menu()<CR>')
+vim.keymap.set('n', "<S-l>", ':lua require("harpoon.ui").nav_next()<CR>')
+vim.keymap.set('n', "<S-h>", ':lua require("harpoon.ui").nav_prev()<CR>')
