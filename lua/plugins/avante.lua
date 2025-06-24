dd43fc20de4d1b927e1cf68c359535324c5c9022
vvalntnv <vvalntnv@gmail.com>
2025-06-24 10:20:24 +0300

    changed avante position

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -63,7 +63,7 @@ return {
 		},
 		windows = {
 			---@type "right" | "left" | "top" | "bottom"
-			position = "left", -- the position of the sidebar
+			position = "right", -- the position of the sidebar
 			wrap = true, -- similar to vim.o.wrap
 			width = 30, -- default % based on available width
 			sidebar_header = {
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -28,15 +28,15 @@ return {
 					["<Tab>"] = cmp.mapping(function(fallback)
 						if cmp.visible() then
 							cmp.confirm({ select = true })
-						elseif luasnip.expand_or_jumpable() then
-							luasnip.expand_or_jump()
+						elseif luasnip.locally_jumpable(1) then
+							luasnip.jump(1)
 						else
 							fallback()
 						end
 					end, { "i", "s" }),
 
 					["<S-Tab>"] = cmp.mapping(function(fallback)
-						if luasnip.expand_or_jumpable() then
+						if luasnip.locally_jumpable(-1) then
 							luasnip.jump(-1)
 						else
 							fallback()
