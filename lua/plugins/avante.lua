aa200839b5cc98afa8d438ffbeee904ec2e0d1a0
Viktor Valentinov <vvalentinov@neterra.net>
2025-08-01 14:50:36 +0300

    avante new model yayayayayyaya

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -14,13 +14,13 @@ return {
 		providers = {
 			claude = {
 				endpoint = "https://api.anthropic.com",
-				model = "claude-3-7-sonnet-20250219", -- your desired model (or use gpt-4o, etc.)
+				model = "claude-sonnet-4-20250514", -- your desired model (or use gpt-4o, etc.)
 				timeout = 30000,             -- Timeout in milliseconds, increase this for reasoning models
 				--reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
 			},
 			extra_request_body = {
 				temperature = 0,
-				max_tokens = 4096,           -- Increase this to include reasoning tokens (for reasoning models)
+				max_tokens = 8096,           -- Increase this to include reasoning tokens (for reasoning models)
 			}
 		},
 		mappings = {
